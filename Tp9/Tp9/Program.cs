c9ff12cd7085af42843dd1a16c57ed0c3ac7d7d7
Enzo <enzobulacio18@gmail.com>
2020-07-07 15:09:35 -0300

    guardar datos

--- Tp9/Tp9/Program.cs
+++ Tp9/Tp9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Tp9
 {
@@ -6,7 +7,21 @@ namespace Tp9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string ruta = Directory.GetCurrentDirectory();
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion(ruta);
+            string Directorio = SoporteParaConfiguracion.LeerConfiguracion(ruta);
+            if (Directory.Exists(ruta))
+            {
+                Directory.CreateDirectory(ruta + @"morse\");
+            }
+            Console.WriteLine("se creo el archivo" + Directorio + "datos guardados");
+            Console.Write("ingrese una oracion");
+            string aTra = Console.ReadLine();
+            //texto a morse
+            string TextToMorse = ConversorDeMorse.TextoAMorse(aTra, ToUpper());
+            Console.WriteLine("\nLa traduccion se guardo"+ );
         }
+
+
     }
 }
